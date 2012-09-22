8b0438acf85957845e5ce1994684fda5482076c6
Akihiro Maeda <sola.1980.a@gmail.com>
2012-09-22 16:06:20 +0900

    Add Root access. Add JCROM version.

---
+++ common.mk
@@ -0,0 +1,17 @@
+PRODUCT_VERSION_MAJOR = 0
+PRODUCT_VERSION_MINOR = 9
+PRODUCT_VERSION_MAINTENANCE = 0
+
+JCROM_VERSION := JCROM-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(PRODUCT_VERSION_MAINTENANCE)-$(shell date -u +%Y%m%d)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.jcrom.version=$(JCROM_VERSION)
+
+PRODUCT_COPY_FILES +=  \
+    jcrom/common/prebuilt/apps/Superuser.apk:system/app/Superuser.apk
+
+PRODUCT_PACKAGES += \
+    Superuser \
+    Superuser.apk \
+    su
+
