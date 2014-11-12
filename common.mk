dca093cf03356424eecff73af29088401eb732ec
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-13 01:15:41 +0900

    Updated common.mk for Lollipop

--- common.mk
+++ common.mk
@@ -1,34 +1,31 @@
 include $(TOPDIR)jcrom/common/version.mk
 
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.jcrom.version=$(JCROM_VERSION)
+    ro.jcrom.version=$(JCROM_VERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
 PRODUCT_PACKAGES += \
-    jctool \
-    bootsound \
-    stagefright
-
-PRODUCT_PACKAGES += \
-    busybox
+    japanesekeyboard_1.1
 
 PRODUCT_PACKAGES += \
     VisualThemeSelector \
     JCWallpaper \
-    JCLauncher
+    JCLauncher \
+    jctool
 
-PRODUCT_PACKAGES += \
-    Browser
-
-PRODUCT_PACKAGES += \
-    audio_effects.conf
+PRODUCT_PACKAGE_OVERLAYS += \
+    jcrom/common/overlay
 
 PRODUCT_COPY_FILES +=  \
-    jcrom/common/init.jcrom.rc:root/init.jcrom.rc \
-    jcrom/common/prebuilt/apps/japanesekeyboard/japanesekeyboard_1.1.apk:system/app/japanesekeyboard_1.1.apk
+    jcrom/common/init.jcrom.rc:root/init.jcrom.rc
 
-PRODUCT_COPY_FILES += jcrom/common/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES += \
+    jcrom/common/prebuilt/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-PRODUCT_COPY_FILES += jcrom/common/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
+PRODUCT_COPY_FILES += \
+    jcrom/common/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=1
